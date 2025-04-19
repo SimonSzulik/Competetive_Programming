9a1940aef185721bdde4cdb4484bb010ccce28f2
SimonSzulik <SimonSzulik21@gmail.com>
2025-04-19 14:51:46 +0200

    Lösung für event, palindrome, hello

---
+++ Exercises/Week1/hello/hello.cpp
@@ -0,0 +1,23 @@
+// einmal mit allem bitte
+#include <bits/stdc++.h>
+// wer will schon immer std:: schreiben
+using namespace std;
+// schreibfaul aber wissen was drin ist
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint32_t u32;
+typedef uint64_t u64;
+// besser ist das
+#define float fliesskommazahlensindboese
+#define double einfachnichtbenutzen
+// und los
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string name;
+    cin >> name;
+    cout << "Hello "
+         << name << "!" << "!\n";
+}
